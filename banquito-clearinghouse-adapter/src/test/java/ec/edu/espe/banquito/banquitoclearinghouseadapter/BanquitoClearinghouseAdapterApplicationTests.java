925eb392c6ad3c97653856fe303267ed7ee84602
Anahy Herrera <aeherrera16@espe.edu.ec>
2026-06-21 18:27:38 -0500

    fix: corregir property de conexion Mongo en test (spring.mongodb.uri, no spring.data.mongodb.uri en Boot 4.0.6)

--- banquito-clearinghouse-adapter/src/test/java/ec/edu/espe/banquito/banquitoclearinghouseadapter/BanquitoClearinghouseAdapterApplicationTests.java
+++ banquito-clearinghouse-adapter/src/test/java/ec/edu/espe/banquito/banquitoclearinghouseadapter/BanquitoClearinghouseAdapterApplicationTests.java
@@ -4,7 +4,7 @@ import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
 
 @SpringBootTest(properties = {
-        "spring.data.mongodb.uri=mongodb://localhost:27017/clearingdb"
+        "spring.mongodb.uri=mongodb://localhost:27017/clearingdb"
 })
 class BanquitoClearinghouseAdapterApplicationTests {
 
